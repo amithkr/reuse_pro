269038f5de8c3bb5561f368324d306b6d2553dbb
Amith k <amithkalathil@gmail.com>
2019-05-05 14:35:19 +0530

    renamed folders, removed padding for body

--- reuse/urls.py
+++ reuse/urls.py
@@ -1,4 +1,4 @@
-"""tutorial1 URL Configuration
+"""reuse URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- reuse/wsgi.py
+++ reuse/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tutorial1 project.
+WSGI config for reuse project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reuse.settings")
 
 application = get_wsgi_application()
